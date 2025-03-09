edd6de52020dc08f23e1aeae64e3b7e452e706f1
Peace <khann@onechain.game>
2025-03-09 19:21:44 +0700

    first commit

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,11 @@
+// using UnityEngine;
+//
+// public class CharacterController : MonoBehaviour
+// {
+//     public bool isGrounded = true;
+//
+//     public void Move(Vector3 movement)
+//     {
+//         
+//     }
+// }
